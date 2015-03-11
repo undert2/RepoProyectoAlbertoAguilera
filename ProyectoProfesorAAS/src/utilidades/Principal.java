389b804f47d1c676307b7ca1db6865d72b1bc9c5
Alberto <undert2@hotmail.com>
2015-03-11 09:52:07 +0100

    Segundo commit

--- ProyectoProfesorAAS/src/utilidades/Principal.java
+++ ProyectoProfesorAAS/src/utilidades/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("PRÁCTICAS ENTORNOS DE DESARROLLO");
 		System.out.println(" <---//Este es el proyecto de Alberto Aguilera Sanz\\\\---> ");
+		System.out.println("Modificacion 1");
 
 	}
 
---
+++ ProyectoProfesorAAS/src/utilidades/Segunda_Clase.java
@@ -0,0 +1,10 @@
+package utilidades;
+
+public class Segunda_Clase {
+
+	public static void main(String[] args) {
+		System.out.println("Esta es la segunda parte del repositorio");
+
+	}
+
+}
